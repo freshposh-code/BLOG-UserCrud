c67b786a8327ed2e6658356ed32d703553afb8d8
freshposh-code <atomisefarouk919@gmail.com>
2024-08-23 02:40:46 +0100

    set user registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function register()
+    public function register(Request $request)
     {
-        return 'Registered successfully';
+        $incomingFields = $request->validate([
+            'name' => ['required', 'min:3', 'max:10', Rule::unique('users', 'name')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'max:100']
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
     }
-};
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BlogApp</title>
 </head>
 
 <body>
+
+    @auth
+    <h1 style="text-align: center">Congrats, you are logged in</h1>
+    @else
     <h1 style="text-align: center;">Register Page</h1>
     <div style="display: flex; justify-content: center; align-items: center; min-height: 100vh;">
         <form action="/register" method="POST" style="display: flex; flex-direction: column; width: 550px; border: 1px solid black; padding: 25px;">
             @csrf
             <h3>Name:</h3>
-            <input style="padding: 6px 10 6px 10; outline: none;" type="text" placeholder="name">
+            <input name="name" value="{{ old('name') }}" style="padding: 6px 10px; outline: none; border: 1px solid black" type="text" placeholder="name">
+            <!-- Display error message for name field -->
+            @if ($errors->has('name'))
+                <span style="color: red;">{{ $errors->first('name') }}</span>
+            @endif
+
             <h3>Email:</h3>
-            <input style="padding: 6px 10 6px 10;  outline: none;" type="email" placeholder="email">
+            <input name="email" value="{{ old('email') }}" style="padding: 6px 10px; outline: none;" type="email" placeholder="email">
+            <!-- Display error message for email field -->
+            @if ($errors->has('email'))
+                <span style="color: red;">{{ $errors->first('email') }}</span>
+            @endif
+
             <h3>Password:</h3>
-            <input style="padding: 6px 10 6px 10;  outline: none;" type="text" placeholder="password">
+            <input name="password" style="padding: 6px 10px; outline: none;" type="password" placeholder="password">
+            <!-- Display error message for password field -->
+            @if ($errors->has('password'))
+                <span style="color: red;">{{ $errors->first('password') }}</span>
+            @endif
 
             <button style="margin-top: 15px; padding: 10px; background: transparent; font-weight: bold; font-size: medium; cursor: pointer;">Register</button>
         </form>
     </div>
+
+    @endauth
+    
 </body>
 
 </html>
