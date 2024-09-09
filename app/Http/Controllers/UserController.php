f7dc77c213201b8a1f005717b775eacec7ddc205
freshposh-code <atomisefarouk919@gmail.com>
2024-09-09 16:29:33 +0100

    update on users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function usersCoolPosts()
+    {
+        return $this->hasMany(Post::class, 'user_id');
+    }
 }
